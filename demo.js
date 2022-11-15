9b5321ca991694e3476f2cf8d3df16e95a074377
mudmaudo <117263153+mudmaudo@users.noreply.github.com>
2022-11-15 20:53:19 +0700

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,13 @@
+{
+    document.getElementById(theImage).src="http://31.media.tumblr.com/fca646cd8fe87906e605ad7e8d039903/tumblr_mmoz4fWT6U1soh1p8o1_500.png";
+    }
+    let btn1=document.querySelector('#light1');
+    let img1= document.querySelector('.light')
+    let btn2=document.querySelector('#light2');
+
+    btn1.addEventListener('click',()=>{
+        img1 ='air-conditioner-svgrepo-com1.svg';
+    })
+    btn2.addEventListener('click',()=>{
+        img1 ='air-conditioner-svgrepo-com.svg';
+    })
